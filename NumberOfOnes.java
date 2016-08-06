c087cb51498a20f4694970b3955599c54f7030d2
pcoder99 <susan.2010hunter@gmail.com>
2016-08-05 21:13:02 -0700

    Update NumberOfOnes.java

--- NumberOfOnes.java
+++ NumberOfOnes.java
@@ -5,3 +5,12 @@ public static int countOnes(int n){
     num++;
   }
 }
+
+public static int countOnes(int n){
+  int num = 0;
+  while(n > 0){
+    num += n& 0x01;
+    n >>= 1;
+  }
+  return num;
+}
